d3e3ac530fa05064d2aee30d89c72e529941a46e
NicolleRG <cnrebazag@unitru.edu.pe>
2021-07-21 11:18:50 -0500

    add information-Profile

--- src/lib/profile.js
+++ src/lib/profile.js
@@ -1,8 +1,105 @@
 export const profile = () => {
-  const container = document.createElement('div');
-  const notas = `
-      <p>Profile!!!!</p>
+    const containerProfile = document.createElement('section');
+    const informationProfile = document.createElement('header');
+    const mainProfile = document.createElement('main');
+
+    const contentInformationProfile = ` 
+    
+    <div class="information-profile">
+        
+        <div class="profile-photo">
+            <img class="photo" src="https://www.mdzol.com/u/fotografias/m/2020/12/5/f848x477-989576_1047379_5050.jpg">
+        </div>
+        <label id="select-profile" for="select-photo-profile">
+            <input type="file" id="select-photo-profile" class="hide" accept="image/jpeg, image/png">
+            <span class="edit-photo"><span class="tooltiptext">Cambiar</span></span>
+        </label>
+
+        <h2>Nombre:</h2>
+            <h2 class="user-name"> Maria Rivero </h2>
+
+        <h2>Sobre mi</h2>
+            <p name="description">Hola, me gusta programar, comer y bailar</p>
+
+        <h2>Sigueme</h2>
+            <p name="links"> Mis redes sociales , cgr1234 </p>
+    
+    </div>
     `;
-  container.innerHTML = notas;
-  return container;
+
+    const contentMainProfile = `
+    
+    <header class="edition-information">
+      <button type="button" class="edit-info" id="btn-editProfile"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+      <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+    </svg><span class="tooltiptext">Editar Perfil</span>
+      </button>
+
+      <form class="editProfile">
+        <div class="grupo">
+        <label  for="usernameEdit">Nombre:</label>
+        <input type="text" id="usernameEdit" value=" ">
+        </div>
+        <div class="grupo1">
+          <label  for="aboutMeEdit">Sobre mi:</label>
+          <textarea id="aboutMeEdit"> </textarea>
+        </div>
+        <div class="grupo2">
+          <label  for="followMeEdit">Sigueme</label>
+          <input type="text" id="followMeEdit" value=" ">
+        </div>
+        <button type="submit" class="btn-update">Actualizar</button>
+      </form>
+
+        
+      <span class = "profile-privacy" id="btn-privacy"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgb(0, 0, 0); --darkreader-inline-fill:#a2a19f;" data-darkreader-inline-fill=""><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zM4 12c0-.899.156-1.762.431-2.569L6 11l2 2v2l2 2 1 1v1.931C7.061 19.436 4 16.072 4 12zm14.33 4.873C17.677 16.347 16.687 16 16 16v-1a2 2 0 0 0-2-2h-4v-3a2 2 0 0 0 2-2V7h1a2 2 0 0 0 2-2v-.411C17.928 5.778 20 8.65 20 12a7.947 7.947 0 0 1-1.67 4.873z"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgb(0, 0, 0); --darkreader-inline-fill:#a2a19f;" data-darkreader-inline-fill=""><path d="m11.998 17 7-8h-14z"></path></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgb(0, 0, 0); --darkreader-inline-fill:#a2a19f;" data-darkreader-inline-fill=""><path d="M20 12c0-1.103-.897-2-2-2h-1V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v3H9V7z"></path></svg></span>
+    </header>
+    
+    //Aside left
+    <aside class="aside-left">
+        <ul>
+            <li><a href="http://xxxx"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+            <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+          </svg>Mis aliadas</a></li>
+            <li><a href="http://x"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-square-fill" viewBox="0 0 16 16">
+            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm.5 5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1 0-1zM4 8.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm2 3a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5z"/>
+          </svg>Mis post</a></li>
+            <li><a href="http://xxx"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+          </svg>Mi frase favorita es:</a></li>
+        </ul>
+    </aside>
+
+    // Add post 
+    <main>
+   
+    </main>
+    
+    //Aside right
+    <aside class="aside-right">
+        <ul>
+            <li><a href="http://xxxx"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-paperclip" viewBox="0 0 16 16">
+            <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z"/>
+            </svg>Mis archivos</a></li>
+            <li><a href="http://xxx"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>Mis eventos</a></li>
+            <li><a href="http://xxxx"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code" viewBox="0 0 16 16">
+            <path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z"/>
+            </svg>
+            </svg>Mis códigos</a></li>
+        </ul>
+    </aside>
+
+    `;
+
+
+    informationProfile.innerHTML = contentInformationProfile;
+    mainProfile.innerHTML= contentMainProfile;
+    containerProfile.appendChild(informationProfile);
+    containerProfile.appendChild(mainProfile);
+
+    return containerProfile;
+
+    
 };
